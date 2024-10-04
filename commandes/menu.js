b3e183f0380990fd57b30a5e150bf0ac7f06e533
Bbeltah <briankenya426@gmail.com>
2024-10-04 19:43:27 +0300

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -27,65 +27,72 @@ zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions
         coms[com.categorie].push(com.nomCom);
     });
 
-    moment.tz.setDefault('Eat/GMT');
+    moment.tz.setDefault('Africa/Nairobi');
 
 // Créer une date et une heure en GMT
 const temps = moment().format('HH:mm:ss');
-const date = moment().format('DD-MM-YYYY');
+const date = moment().format('DD/MM/YYYY');
 
     let infoMsg =  `
-┏▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭
-▮ 『𝗠𝗔𝗜𝗡』
-▯  🌐 *Work Type* : 【${mode} mode】
-▮  🔑 *Starter* : [${s.PREFIXE}]
-▯  📜 Plugins : [${cm.length}0] 
-▮  ⌚️ *Time* :  ${temps}
-▯  🖥️ *System* : [ Whiskeysockets ]
-▮  🌌 *Theme* : [ Ultra 5G speed ]
-▯  █▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
-└▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭
-┏────────────────⊷
-┃ 『𝗦𝗧𝗢𝗥𝗔𝗚𝗘』
-┃  📼 *Used Ram* : ${format(os.totalmem() - os.freemem())}
-┃  📼 *Total Ram* :${format(os.totalmem())}
-└─────═━┈┈━═─────⊷
-╭━━━━∙⋆⋅⋆∙━ ─┉─ • ─┉─⊷
-┇ 『𝗖𝗢𝗡𝗧』
-┇  🕵 *Dev* : 𝐁𝐞𝐥𝐭𝐚𝐡 𝐓𝐞𝐜𝐡 𝟐𝟓𝟒 🇰🇪
-┇  📱 *User* : ${s.OWNER_NAME}
-└▪︎─═━┈━═─ ═▪︎─═━┈━═─⊷
-╒✦•··············•••••••••··············•··•✦
-┇ 『𝗕𝗢𝗧 𝗦𝗧𝗔𝗧𝗨𝗦』
-┇   ─┉┈◈◉◈┈┉
-┇  📡 *Platform* : ${os.platform}
-┇  🎲 *Status* : Ultimate speed Bot
-┇  🗨️ *Series* : [Version 4 lite]
-╘✦•·············•••••••••··················•✦\n${readmore}
+╭─────═[ 𝐒𝐂𝐄𝐍𝐄-𝐌𝐃 ]═─────⋆
+├━━━━━━━━━━━━━━━━┈─⋆
+│┠─═[ 𝗠𝗔𝗜𝗡 ]
+│╭───────────────···
+┴│▸*Bot Mode* : 【${mode} mode】
+⬡│▸ *Owner* : ${s.OWNER_NAME}
+⬡│▸ *Prefix* : [${s.PREFIXE}]
+⬡│▸ *Plugins* : [ 245 ] 
+⬡│▸ *Time* :  ${temps}
+⬡│▸ *Today* : [ ${date} ]
+⬡│▸ *Time Zone* : [ Africa/Nairobi ] 
+┬╰────────────────···
+├━━━━━━━━━━━━━━━━┈─⋆
+│┠─═[ 𝗦𝗧𝗢𝗥𝗔𝗚𝗘 ]
+│╭───────────────···
+┴│▸ *Used Ram* : ${format(os.totalmem() - os.freemem())}
+⬡│▸  *Total Ram* :${format(os.totalmem())}0
+┬╰────────────────···
+├━━━━━━━━━━━━━━━━┈─⋆
 `;
 let menuMsg =  `
-▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣
- 
-◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢
+│ ┠─═[ 𝗖𝗢𝗡𝗧 ] 
+│╭───────────────···
+┴│▸ *Bot-User* : *${nomAuteurMessage}*
+┬╰────────────────···
+├━━━━━━━━━━━━━━━━┈─⋆
+│┠─═[ 𝗕𝗢𝗧 𝗦𝗧𝗔𝗧𝗨𝗦 ]
+│╭───────────────···
+┴│▸ *Platform* : Chrome Linux
+⬡│▸  *Status* : Best Wa.Bot
+┬╰────────────────···
+├━━━━━━━━━━━━━━━━┈─⋆
+│┠─═[ *SUPPORT LINKS* ]
+┬╭───────────────···
+⬡ *FOLLOW OUR CHANNEL*
+│▸ whatsapp.com/channel/0029VaRHDBKKmCPKp9B2uH2F
+⬡ *JOIN OUR GROUP*
+│▸ chat.whatsapp.com/CQvUnqaDK4fBjgMXvFEAsI
+⬡ *TELEGRAM LINK*
+│▸ t.me/beltah254 
+┬╰────────────────···
+├━━━━━━━━━━━━━━━━┈─⋆
+╘✦•·············•••••••••··················•✦\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʙᴇʟᴛᴀʜ ʜᴀᴄᴋɪɴɢ ᴛᴇᴀᴍ🇰🇪\n\n${readmore}
 𝐒𝐂𝐄𝐍𝐄-𝐌𝐃 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒
 `;
     
     for (const cat in coms) {
         menuMsg += `
-╭═══❂ *${cat}* ❂`;
+╭═[ *${cat}* ]═────···\n┴`;
         for (const cmd of coms[cat]) {
             menuMsg += `          
-┃ ❏  *${cmd}* `    
+│✧⪼ *${cmd}* `    
         } 
         menuMsg +=`
-╰═══════════⊷
+┬\n╰───────────···
 `
     }
   
        menuMsg += `
-
-➤ 𝐁𝐨𝐭 𝐝𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫
-> 𝐀 𝐩𝐫𝐨𝐝𝐮𝐜𝐭 𝐨𝐟 𝐁𝐞𝐥𝐭𝐚𝐡 𝐓𝐞𝐜𝐡 𝐓𝐞𝐚𝐦
-> 𝐂𝐫𝐞𝐚𝐭𝐞𝐝 𝐛𝐲 𝐁𝐞𝐥𝐭𝐚𝐡 𝐊𝐞 𝐅𝐫𝐨𝐦 𝐊𝐞𝐧𝐲𝐚 
 
 𝗧𝗛𝗔𝗡𝗞 𝗬𝗢𝗨 𝗙𝗢𝗥 𝗟𝗢𝗩𝗜𝗡𝗚 𝗦𝗖𝗘𝗡𝗘-𝗠𝗗
 `;
