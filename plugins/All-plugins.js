06261144cf2f18843f7bd35c3bf039f3630cc2e2
Beltah Tech👻 <devbeltah87@gmail.com>
2025-01-03 14:26:53 +0300

    Beltah Tech 254 🇰🇪

---
+++ plugins/All-plugins.js
@@ -0,0 +1,13 @@
+//BELTAH TECH 254 
+
+Consol.log 
+  > remote.scrip-commands/beltah-plugins
+
+else
+
+error.
+  Detected.No.public-commands found!!!
+
+
+
+//Hell 😁 I'm unstopable now.    BELTAH TECH 254 👻
